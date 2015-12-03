8a5820efed809cecdc1ecf58a9e3668e8524165b
Shyamini Kularathna <srhgk2@ks207r14.eng.cam.ac.uk>
2015-12-03 15:14:18 +0000

    added comments and improved algorithm

--- tests/node_base_test.cpp
+++ tests/node_base_test.cpp
@@ -6,43 +6,89 @@
 
 #if GTEST_HAS_PARAM_TEST
 
+//! Build 1D node
+template<typename tNode>
+tNode build_1D_testNode() {
+    const unsigned testId_ = 1;
+    std::array<double, 1> testCoord_1D = { {1.5} };
+    tNode testNode_1D = std::make_shared<felib::NodeBase<1>>(testId_, testCoord_1D);
+    return testNode_1D;
+}
+
+//! Build 2D node
+template<typename tNode>
+tNode build_2D_testNode() {
+    const unsigned testId_ = 2;
+    std::array<double, 2> testCoord_2D = { {1.5} };
+    tNode testNode_2D = std::make_shared<felib::NodeBase<2>>(testId_, testCoord_2D);
+    return testNode_2D;
+}
+
+//! Build 3D node
+template<typename tNode>
+tNode build_3D_testNode() {
+    const unsigned testId_ = 3;
+    std::array<double, 3> testCoord_3D = { {1.5} };
+    tNode testNode_3D = std::make_shared<felib::NodeBase<3>>(testId_, testCoord_3D);
+    return testNode_3D;
+}
+
+// fixture class for each value-parametrized test 
+//! \tparam int Dimension
 class NodeBaseTest : public ::testing::TestWithParam<int> {
 protected:
-    virtual void SetUp() {}
+    virtual void SetUp() {
+      tId_ = 0; }
+
+  void build_testNode() {
+    if (GetParam() == 1) {
+      auto tNode_1D = build_1D_testNode<std::shared_ptr<felib::NodeBase<1>>>();
+      tId_ = tNode_1D->id();
+      tCoord_1D = tNode_1D->coordinates();
+    }
+    else if (GetParam() == 2) {
+      auto tNode_2D = build_2D_testNode<std::shared_ptr<felib::NodeBase<2>>>();
+      tId_ = tNode_2D->id();
+      tCoord_2D = tNode_2D->coordinates();
+    }
+    else if (GetParam() == 3) {
+      auto tNode_3D = build_3D_testNode<std::shared_ptr<felib::NodeBase<3>>>();
+      tId_ = tNode_3D->id();
+      tCoord_3D = tNode_3D->coordinates();
+    }
+  }
 
     virtual void TearDown() {
-        delete testNode_1D;
-        delete testNode_2D;
-        delete testNode_3D;
-    }
+      tId_ = 0; }
 
-// Need to add an algorith which give the corresponding node for each GetParam()
-// value (i.e. for each dimension)
-
-protected:
-    felib::NodeBase<1>* testNode_1D = new felib::NodeBase<1>(testId_, testCoord_1D);
-    felib::NodeBase<2>* testNode_2D = new felib::NodeBase<2>(testId_, testCoord_2D);
-    felib::NodeBase<3>* testNode_3D = new felib::NodeBase<3>(testId_, testCoord_3D);
-    const unsigned testId_ = 0;
-    std::array<double, 1> testCoord_1D = { {1.5} };
-    std::array<double, 2> testCoord_2D = { {1.5} };
-    std::array<double, 3> testCoord_3D = { {1.5} };
-
+    const unsigned tId_;
+    std::array<double, 1> tCoord_1D;
+    std::array<double, 2> tCoord_2D;
+    std::array<double, 3> tCoord_3D;
 };
 
-TEST_P(NodeBaseTest, nodeId) { 
-    unsigned num = 1;
-    EXPECT_EQ(1, num);
+// value-parametrized test 
+//! tests the 'id' function
+TEST_P(NodeBaseTest, id) { 
+  build_testNode();
+  EXPECT_EQ(tId_, GetParam());
 }
 
+// value-parametrized test
+//! tests the 'coordinates' function
 TEST_P(NodeBaseTest, coordinates) {
-    unsigned num = 1;
-    EXPECT_EQ(1, num);
+  build_testNode();
 }
 
+// input parameters for NodeBaseTest fixture class
+//! \tparam 1 1D
+//! \tparam 2 2D
+//! \tparam 3 3D
 INSTANTIATE_TEST_CASE_P(1D2Dand3D, NodeBaseTest, ::testing::Values(1, 2, 3));
+
 #endif // GTEST_HAS_PARAM_TEST
 
+//! Run each test
 int main(int arc, char* arv[]) {
     ::testing::InitGoogleTest(&arc, arv);
     return RUN_ALL_TESTS();
