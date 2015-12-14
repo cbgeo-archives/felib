ccefbb000771c61bd1f2971efe5ec4c1897e19e5
Loizos <lp418@cam.ac.uk>
2015-12-14 11:52:13 +0000

    Add node_base.h commented file

--- include/node_base.h
+++ include/node_base.h
@@ -7,19 +7,21 @@
 #include <vector>
 
 namespace felib {
-template <unsigned Tdim> class NodeBase;
+template <unsigned Tdim>
+class NodeBase;
 }
 
 // Node Base class
 //! \brief Base class that stores the information about nodes
-//! \details NodeBase class: id_ and coordinates.
+//! \details NodeBase class: id_ and coordinates. test test test
 //! \tparam Tdim Dimension
-template <unsigned Tdim> class felib::NodeBase {
-public:
+template <unsigned Tdim>
+class felib::NodeBase {
+ public:
   // Constructor with id and coordinates
   //! \param[in] id assign as the id_ of the node
   //! \param[in] coord coordinates of the node
-  NodeBase(const unsigned &id, const std::array<double, Tdim> &coord)
+  NodeBase(const unsigned& id, const std::array<double, Tdim>& coord)
       : id_{id} {
     coordinates_ = coord;
   };
@@ -28,34 +30,34 @@ public:
   virtual ~NodeBase(){};
 
   //! Return id of the node
+  //! \return id_ id of the node
   unsigned id() const { return id_; }
 
   //! Assign coordinates
   //! \param[in] coord Assign coord as coordinates of the node
-  void coordinates(const std::array<double, Tdim> &coord) {
+  void coordinates(const std::array<double, Tdim>& coord) {
     coordinates_ = coord;
   }
 
   //! Return coordinates
-  //! \param[out] coordinates_ return coordinates of the node
+  //! \return coordinates_ return coordinates of the node
   std::array<double, Tdim> coordinates() const { return coordinates_; }
 
   //! Info
   void info() {
     std::cout << "Node id: " << id_ << ", coordinates: ";
-    for (auto coord : coordinates_)
-      std::cout << coord << ", ";
+    for (auto coord : coordinates_) std::cout << coord << ", ";
     std::cout << std::endl;
   }
 
-private:
+ private:
   //! Copy constructor
-  NodeBase(const NodeBase<Tdim> &);
+  NodeBase(const NodeBase<Tdim>&);
 
   //! Assignement operator
-  NodeBase &operator=(const NodeBase<Tdim> &);
+  NodeBase& operator=(const NodeBase<Tdim>&);
 
-protected:
+ protected:
   //! node id
   unsigned id_;
 
@@ -63,4 +65,4 @@ protected:
   std::array<double, Tdim> coordinates_;
 };
 
-#endif // FELIB_SRC_NODE_BASE_H_
+#endif  // FELIB_SRC_NODE_BASE_H_
