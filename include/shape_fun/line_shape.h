fad74cce6100698414885449d7ca323931a70bea
Krishna Kumar <kks32@cam.ac.uk>
2016-02-19 11:27:29 +0000

    include guards modifications

--- include/shape_fun/line_shape.h
+++ include/shape_fun/line_shape.h
@@ -1,5 +1,5 @@
-#ifndef FELIB_SRC_LINESHAPE_H_
-#define FELIB_SRC_LINESHAPE_H_
+#ifndef FELIB_LINESHAPE_H_
+#define FELIB_LINESHAPE_H_
 
 #include <iostream>
 #include <array>
@@ -7,27 +7,30 @@
 
 #include "shapefun_base.h"
 
-// Line shape function class derived from Shape Fun Base Class
+//! \brief Line shape function class derived from Shape Fun Base Class
 //! \shape functions of the line element
 //! \tparam Tdim Dimenstion
 //! \tparam numFun number of shape functions
 namespace felib {
-  template <unsigned Tdim, unsigned numFun> class LineShape;
+template <unsigned Tdim, unsigned numFun> class LineShape;
 }
 
-template <unsigned Tdim, unsigned numFun> class felib::LineShape :
-  public felib::ShapeFunBase <unsigned Tdim, unsigned numFun> {
+template <unsigned Tdim, unsigned numFun>
+class felib::LineShape
+    : public felib::ShapeFunBase<unsigned Tdim, unsigned numFun> {
 
-  void evaluate_shape_fun(const std::array<double, Tdim> &xi, std::array<double, numFun> &sFun);
+  void evaluate_shape_fun(const std::array<double, Tdim> &xi,
+                          std::array<double, numFun> &sFun);
 
-  void evaluate_grad_shape_fun(const std::array<double, Tdim> &xi, std::array<std::array<double, numFun>, Tdim> &gradSfun);
+  void evaluate_grad_shape_fun(
+      const std::array<double, Tdim> &xi,
+      std::array<std::array<double, numFun>, Tdim> &gradSfun);
 
   void evaluate_jacobian();
 
   void quadrature_points();
-
 };
 
-#include "line_shape.ipp"
+#include "line_shape.tcc"
 
-#endif // FELIB_SRC_LINESHAPE_H_
+#endif // FELIB_LINESHAPE_H_
