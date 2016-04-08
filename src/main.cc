8ca50100338f3b620415ca61ba56ae810ea149b2
Krishna Kumar <kks32@cam.ac.uk>
2016-04-08 23:57:38 +0100

    quadrature base class and linequadrature

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include "node_base.h"
 #include "shapefun_base.h"
-#include "triangle_shape.h"
+#include "line_shape.h"
+#include "line_quadrature.h"
 
 #include <array>
 #include <iostream>
@@ -8,11 +9,17 @@
 
 int main(int argc, char** argv) {
   unsigned id = 0;
-  const unsigned Dim = 2;
+  const unsigned Dim = 1;
   std::array<double, Dim> coord = {{0.}};
 
   auto node = std::make_shared<felib::NodeBase<Dim>>(id, coord);
   node->info();
 
-  auto triangle = std::make_shared<felib::TriangleShape<Dim, 3>>();
+  auto line = std::make_shared<felib::LineShape<Dim, 2>>();
+
+  auto quad = std::make_shared<felib::LineQuadrature<Dim, 2>>();
+
+  auto points = quad->quadratures();
+
+  std::cout << points << std::endl;
 }
