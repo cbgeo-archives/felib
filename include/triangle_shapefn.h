02fbc16d5c265547e0e07bd20bec58f3fe4600f5
Krishna Kumar <kks32@cam.ac.uk>
2016-04-11 19:31:47 +0100

    Triangle shapefn test cases

---
+++ include/triangle_shapefn.h
@@ -0,0 +1,47 @@
+#ifndef FELIB_TIANGLESHAPEFN_H_
+#define FELIB_TIANGLESHAPEFN_H_
+
+#include <eigen3/Eigen/Dense>
+
+#include <array>
+#include <iostream>
+#include <vector>
+
+#include "shapefn_base.h"
+
+// Triangle shape function class derived from ShapeFnBase class
+//! \brief shape functions of the triangle element
+//! \tparam Tdim Dimension
+//! \tparam Tnfunctions Number of shape functions
+namespace felib {
+template <unsigned Tdim, unsigned Tnfunctions>
+class TriangleShapeFn;
+}
+
+template <unsigned Tdim, unsigned Tnfunctions>
+class felib::TriangleShapeFn : public felib::ShapeFnBase<Tdim, Tnfunctions> {
+
+ public:
+  // Default constructor
+  TriangleShapeFn() : felib::ShapeFnBase<Tdim, Tnfunctions>() {
+    static_assert(Tdim == 2, "Invalid dimension for a triangle element");
+    static_assert((Tnfunctions == 3 || Tnfunctions == 6),
+                  "Invalid shape functions for a quadirlateral element");
+  }
+
+  //! Evaluate the shape functions
+  //! \param[in] xi Local coordinates
+  //! \param[out] shapefn_ Shape functions at the given local coordinates
+   Eigen::Matrix<double, Tnfunctions, 1> shapefn(
+         const std::array<double, Tdim>& xi);
+
+  //! Evaluate the gradient of shape functions in local coordinates
+  //! \param[in] xi Local coordinates
+  //! \param[out] grad_shapefn_ Local gradients of shape functions
+  Eigen::Matrix<double, Tnfunctions, Tdim> grad_shapefn(
+         const std::array<double, Tdim>& xi);
+};
+
+#include "triangle_shapefn.tcc"
+
+#endif  // FELIB_TIANGLESHAPEFN_H_
