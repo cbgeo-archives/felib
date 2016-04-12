675edbe4009969d7f98190ac3b2aea8bac9ae9d3
Krishna Kumar <kks32@cam.ac.uk>
2016-04-12 09:27:52 +0100

    1D line shape function test

--- tests/line_shapefn_test.cc
+++ tests/line_shapefn_test.cc
@@ -35,16 +35,16 @@ TEST_CASE("Line shape functions are checked", "[linesf][line][shapefn][1D]") {
       REQUIRE(gradsf(1) == Approx(0.5).epsilon(Tolerance));
     }
     
-    // Coordinates is (-1);
-    SECTION("Two noded line shape function for coordinates(-1)") {
-      std::array<double, Dim> coords = {{ -1. }};
+    // Coordinates is (-0.5);
+    SECTION("Two noded line shape function for coordinates(-0.5)") {
+      std::array<double, Dim> coords = {{ -0.5 }};
       auto shapefn = linesf->shapefn(coords);
 
       // Check shape function
       REQUIRE(shapefn.size() == Nfunctions);
 
-      REQUIRE(shapefn(0) == Approx(1.0).epsilon(Tolerance));
-      REQUIRE(shapefn(1) == Approx(0.).epsilon(Tolerance));
+      REQUIRE(shapefn(0) == Approx(0.75).epsilon(Tolerance));
+      REQUIRE(shapefn(1) == Approx(0.25).epsilon(Tolerance));
 
       // Check gradient of shape functions
       auto gradsf = linesf->grad_shapefn(coords);
@@ -56,16 +56,16 @@ TEST_CASE("Line shape functions are checked", "[linesf][line][shapefn][1D]") {
       REQUIRE(gradsf(1) == Approx(0.5).epsilon(Tolerance));
     }
     
-    // Coordinates is (1)
-    SECTION("Two noded line shape function for coordinates(1,1)") {
-      std::array<double, Dim> coords = {{ 1. }};
+    // Coordinates is (0.5)
+    SECTION("Two noded line shape function for coordinates(0.5)") {
+      std::array<double, Dim> coords = {{ 0.5 }};
       auto shapefn = linesf->shapefn(coords);
 
       // Check shape function
       REQUIRE(shapefn.size() == Nfunctions);
 
-      REQUIRE(shapefn(0) == Approx(0.).epsilon(Tolerance));
-      REQUIRE(shapefn(1) == Approx(1.0).epsilon(Tolerance));
+      REQUIRE(shapefn(0) == Approx(0.25).epsilon(Tolerance));
+      REQUIRE(shapefn(1) == Approx(0.75).epsilon(Tolerance));
 
       // Check gradient of shape functions
       auto gradsf = linesf->grad_shapefn(coords);
@@ -108,17 +108,17 @@ TEST_CASE("Line shape functions are checked", "[linesf][line][shapefn][1D]") {
       REQUIRE(gradsf(2) == Approx(0.0).epsilon(Tolerance));
     }
     
-    // Coordinates is (-1);
-    SECTION("Three noded line shape function for coordinates(-1)") {
-      std::array<double, Dim> coords = {{ -1. }};
+    // Coordinates is (-0.5);
+    SECTION("Three noded line shape function for coordinates(-0.5)") {
+      std::array<double, Dim> coords = {{ -0.5 }};
       auto shapefn = linesf->shapefn(coords);
 
       // Check shape function
       REQUIRE(shapefn.size() == Nfunctions);
 
-      REQUIRE(shapefn(0) == Approx(1.).epsilon(Tolerance));
-      REQUIRE(shapefn(1) == Approx(0.).epsilon(Tolerance));
-      REQUIRE(shapefn(2) == Approx(0.).epsilon(Tolerance));
+      REQUIRE(shapefn(0) == Approx(0.375).epsilon(Tolerance));
+      REQUIRE(shapefn(1) == Approx(-0.125).epsilon(Tolerance));
+      REQUIRE(shapefn(2) == Approx(0.75).epsilon(Tolerance));
 
       // Check gradient of shape functions
       auto gradsf = linesf->grad_shapefn(coords);
@@ -126,31 +126,31 @@ TEST_CASE("Line shape functions are checked", "[linesf][line][shapefn][1D]") {
       REQUIRE(gradsf.cols() == Dim);
 
 
-      REQUIRE(gradsf(0) == Approx(-1.5).epsilon(Tolerance));
-      REQUIRE(gradsf(1) == Approx(-0.5).epsilon(Tolerance));
-      REQUIRE(gradsf(2) == Approx(2.0).epsilon(Tolerance));
+      REQUIRE(gradsf(0) == Approx(-1.0).epsilon(Tolerance));
+      REQUIRE(gradsf(1) == Approx(0.0).epsilon(Tolerance));
+      REQUIRE(gradsf(2) == Approx(1.0).epsilon(Tolerance));
     }
     
-    // Coordinates is (1)
-    SECTION("Three noded line shape function for coordinates(1,1)") {
-      std::array<double, Dim> coords = {{ 1. }};
+    // Coordinates is (0.5)
+    SECTION("Three noded line shape function for coordinates(0.5)") {
+      std::array<double, Dim> coords = {{ 0.5 }};
       auto shapefn = linesf->shapefn(coords);
 
       // Check shape function
       REQUIRE(shapefn.size() == Nfunctions);
 
-      REQUIRE(shapefn(0) == Approx(0.0).epsilon(Tolerance));
-      REQUIRE(shapefn(1) == Approx(1.0).epsilon(Tolerance));
-      REQUIRE(shapefn(2) == Approx(0.0).epsilon(Tolerance));
+      REQUIRE(shapefn(0) == Approx(-0.125).epsilon(Tolerance));
+      REQUIRE(shapefn(1) == Approx(0.375).epsilon(Tolerance));
+      REQUIRE(shapefn(2) == Approx(0.75).epsilon(Tolerance));
 
       // Check gradient of shape functions
       auto gradsf = linesf->grad_shapefn(coords);
       REQUIRE(gradsf.rows() == Nfunctions);
       REQUIRE(gradsf.cols() == Dim);
 
-      REQUIRE(gradsf(0) == Approx(0.5).epsilon(Tolerance));
-      REQUIRE(gradsf(1) == Approx(1.5).epsilon(Tolerance));
-      REQUIRE(gradsf(2) == Approx(-2.0).epsilon(Tolerance));
+      REQUIRE(gradsf(0) == Approx(0.0).epsilon(Tolerance));
+      REQUIRE(gradsf(1) == Approx(1.0).epsilon(Tolerance));
+      REQUIRE(gradsf(2) == Approx(-1.0).epsilon(Tolerance));
     }
     
   }
