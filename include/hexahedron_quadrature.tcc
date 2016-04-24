9da1c73f4233f4e2140aafad0571266722a72979
Krishna Kumar <kks32@cam.ac.uk>
2016-04-24 09:32:42 +0100

    Fixes hexahedron quadratures #9

--- include/hexahedron_quadrature.tcc
+++ include/hexahedron_quadrature.tcc
@@ -17,37 +17,37 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     weights_.at(0) = 8.;
 
   } else if (Tnquadratures == 8) {
-    qpoints_(0, 0) = -std::sqrt(3.) / 3.;
-    qpoints_(0, 1) = -std::sqrt(3.) / 3.;
-    qpoints_(0, 2) = -std::sqrt(3.) / 3.;
+    qpoints_(0, 0) = -1. / std::sqrt(3.);
+    qpoints_(0, 1) = -1. / std::sqrt(3.);
+    qpoints_(0, 2) = -1. / std::sqrt(3.);
 
-    qpoints_(1, 0) = std::sqrt(3.) / 3.;
-    qpoints_(1, 1) = -std::sqrt(3.) / 3.;
-    qpoints_(1, 2) = -std::sqrt(3.) / 3.;
+    qpoints_(1, 0) = 1. / std::sqrt(3.);
+    qpoints_(1, 1) = -1. / std::sqrt(3.);
+    qpoints_(1, 2) = -1. / std::sqrt(3.);
 
-    qpoints_(2, 0) = std::sqrt(3.) / 3.;
-    qpoints_(2, 1) = std::sqrt(3.) / 3.;
-    qpoints_(2, 2) = -std::sqrt(3.) / 3.;
+    qpoints_(2, 0) = 1. / std::sqrt(3.);
+    qpoints_(2, 1) = 1. / std::sqrt(3.);
+    qpoints_(2, 2) = -1. / std::sqrt(3.);
 
-    qpoints_(3, 0) = -std::sqrt(3.) / 3.;
-    qpoints_(3, 1) = std::sqrt(3.) / 3.;
-    qpoints_(3, 2) = -std::sqrt(3.) / 3.;
+    qpoints_(3, 0) = -1. / std::sqrt(3.);
+    qpoints_(3, 1) = 1. / std::sqrt(3.);
+    qpoints_(3, 2) = -1. / std::sqrt(3.);
 
-    qpoints_(4, 0) = -std::sqrt(3.) / 3.;
-    qpoints_(4, 1) = -std::sqrt(3.) / 3.;
-    qpoints_(4, 2) = std::sqrt(3.) / 3.;
+    qpoints_(4, 0) = -1. / std::sqrt(3.);
+    qpoints_(4, 1) = -1. / std::sqrt(3.);
+    qpoints_(4, 2) = 1. / std::sqrt(3.);
 
-    qpoints_(5, 0) = std::sqrt(3.) / 3.;
-    qpoints_(5, 1) = -std::sqrt(3.) / 3.;
-    qpoints_(5, 2) = std::sqrt(3.) / 3.;
+    qpoints_(5, 0) = 1. / std::sqrt(3.);
+    qpoints_(5, 1) = -1. / std::sqrt(3.);
+    qpoints_(5, 2) = 1. / std::sqrt(3.);
 
-    qpoints_(6, 0) = std::sqrt(3.) / 3.;
-    qpoints_(6, 1) = std::sqrt(3.) / 3.;
-    qpoints_(6, 2) = std::sqrt(3.) / 3.;
+    qpoints_(6, 0) = 1. / std::sqrt(3.);
+    qpoints_(6, 1) = 1. / std::sqrt(3.);
+    qpoints_(6, 2) = 1. / std::sqrt(3.);
 
-    qpoints_(7, 0) = -std::sqrt(3.) / 3.;
-    qpoints_(7, 1) = std::sqrt(3.) / 3.;
-    qpoints_(7, 2) = std::sqrt(3.) / 3.;
+    qpoints_(7, 0) = -1. / std::sqrt(3.);
+    qpoints_(7, 1) = 1. / std::sqrt(3.);
+    qpoints_(7, 2) = 1. / std::sqrt(3.);
 
     weights_.at(0) = 1.;
     weights_.at(1) = 1.;
@@ -59,10 +59,12 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     weights_.at(7) = 1.;
 
   } else if (Tnquadratures == 27) {
-    const double Qpoint_a = std::sqrt(3.) / 5.;
+    const double Qpoint_a = std::sqrt(3./ 5.);
     const double Weight_b = 5. / 9.;
     const double Weight_c = 8. / 9.;
 
+    // Along xi(0) = -1 plane
+    // Line xi(1) = -1
     qpoints_(0, 0) = -Qpoint_a;
     qpoints_(0, 1) = -Qpoint_a;
     qpoints_(0, 2) = -Qpoint_a;
@@ -70,11 +72,12 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(1, 0) = -Qpoint_a;
     qpoints_(1, 1) = -Qpoint_a;
     qpoints_(1, 2) = 0.;
-
+    
     qpoints_(2, 0) = -Qpoint_a;
     qpoints_(2, 1) = -Qpoint_a;
     qpoints_(2, 2) = Qpoint_a;
 
+    // Line xi(1) = 0
     qpoints_(3, 0) = -Qpoint_a;
     qpoints_(3, 1) = 0.;
     qpoints_(3, 2) = -Qpoint_a;
@@ -87,6 +90,7 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(5, 1) = 0.;
     qpoints_(5, 2) = Qpoint_a;
 
+    // Line xi(1) = +1
     qpoints_(6, 0) = -Qpoint_a;
     qpoints_(6, 1) = Qpoint_a;
     qpoints_(6, 2) = -Qpoint_a;
@@ -99,6 +103,8 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(8, 1) = Qpoint_a;
     qpoints_(8, 2) = Qpoint_a;
 
+    // Along xi(0) = 0 plane
+    // Line xi(1) = -1
     qpoints_(9, 0) = 0.;
     qpoints_(9, 1) = -Qpoint_a;
     qpoints_(9, 2) = -Qpoint_a;
@@ -111,6 +117,7 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(11, 1) = -Qpoint_a;
     qpoints_(11, 2) = Qpoint_a;
 
+    // Line xi(1) = 0
     qpoints_(12, 0) = 0.;
     qpoints_(12, 1) = 0.;
     qpoints_(12, 2) = -Qpoint_a;
@@ -123,6 +130,7 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(14, 1) = 0.;
     qpoints_(14, 2) = Qpoint_a;
 
+    // Line xi(1) = +1
     qpoints_(15, 0) = 0.;
     qpoints_(15, 1) = Qpoint_a;
     qpoints_(15, 2) = -Qpoint_a;
@@ -135,6 +143,8 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(17, 1) = Qpoint_a;
     qpoints_(17, 2) = Qpoint_a;
 
+    // Along xi(0) = +1 plane
+    // Line xi(1) = -1
     qpoints_(18, 0) = Qpoint_a;
     qpoints_(18, 1) = -Qpoint_a;
     qpoints_(18, 2) = -Qpoint_a;
@@ -147,6 +157,7 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(20, 1) = -Qpoint_a;
     qpoints_(20, 2) = Qpoint_a;
 
+    // Line xi(1) = 0
     qpoints_(21, 0) = Qpoint_a;
     qpoints_(21, 1) = 0.;
     qpoints_(21, 2) = -Qpoint_a;
@@ -159,6 +170,7 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     qpoints_(23, 1) = 0.;
     qpoints_(23, 2) = Qpoint_a;
 
+    // Line xi(1) = +1
     qpoints_(24, 0) = Qpoint_a;
     qpoints_(24, 1) = Qpoint_a;
     qpoints_(24, 2) = -Qpoint_a;
@@ -175,29 +187,29 @@ felib::HexahedronQuadrature<Tdim, Tnquadratures>::HexahedronQuadrature()
     weights_.at(0) = Weight_b * Weight_b * Weight_b;
     weights_.at(1) = Weight_b * Weight_b * Weight_c;
     weights_.at(2) = Weight_b * Weight_b * Weight_b;
-    weights_.at(3) = Weight_b * Weight_b * Weight_c;
+    weights_.at(3) = Weight_b * Weight_c * Weight_b;
     weights_.at(4) = Weight_b * Weight_c * Weight_c;
-    weights_.at(5) = Weight_b * Weight_b * Weight_c;
+    weights_.at(5) = Weight_b * Weight_c * Weight_b;
     weights_.at(6) = Weight_b * Weight_b * Weight_b;
     weights_.at(7) = Weight_b * Weight_b * Weight_c;
     weights_.at(8) = Weight_b * Weight_b * Weight_b;
 
-    weights_.at(9) = Weight_b * Weight_b * Weight_c;
-    weights_.at(10) = Weight_b * Weight_c * Weight_c;
-    weights_.at(11) = Weight_b * Weight_b * Weight_c;
-    weights_.at(12) = Weight_b * Weight_c * Weight_c;
+    weights_.at(9) = Weight_c * Weight_b * Weight_b;
+    weights_.at(10) = Weight_c * Weight_b * Weight_c;
+    weights_.at(11) = Weight_c * Weight_b * Weight_b;
+    weights_.at(12) = Weight_c * Weight_c * Weight_b;
     weights_.at(13) = Weight_c * Weight_c * Weight_c;
-    weights_.at(14) = Weight_b * Weight_c * Weight_c;
-    weights_.at(15) = Weight_b * Weight_b * Weight_c;
-    weights_.at(16) = Weight_b * Weight_c * Weight_c;
-    weights_.at(17) = Weight_b * Weight_b * Weight_c;
+    weights_.at(14) = Weight_c * Weight_c * Weight_b;
+    weights_.at(15) = Weight_c * Weight_b * Weight_b;
+    weights_.at(16) = Weight_c * Weight_b * Weight_c;
+    weights_.at(17) = Weight_c * Weight_b * Weight_b;
 
     weights_.at(18) = Weight_b * Weight_b * Weight_b;
     weights_.at(19) = Weight_b * Weight_b * Weight_c;
     weights_.at(20) = Weight_b * Weight_b * Weight_b;
-    weights_.at(21) = Weight_b * Weight_b * Weight_c;
+    weights_.at(21) = Weight_b * Weight_c * Weight_b;
     weights_.at(22) = Weight_b * Weight_c * Weight_c;
-    weights_.at(23) = Weight_b * Weight_b * Weight_c;
+    weights_.at(23) = Weight_b * Weight_c * Weight_b;
     weights_.at(24) = Weight_b * Weight_b * Weight_b;
     weights_.at(25) = Weight_b * Weight_b * Weight_c;
     weights_.at(26) = Weight_b * Weight_b * Weight_b;
--- tests/hexahedron_quadrature_test.cc
+++ tests/hexahedron_quadrature_test.cc
@@ -56,37 +56,37 @@ TEST_CASE("Hexahedron quadratures are checked",
     REQUIRE(points.cols() == 3);
 
     // Check quadrature points
-    REQUIRE(points(0, 0) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(0, 1) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(0, 2) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(0, 0) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(0, 1) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(0, 2) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
 
-    REQUIRE(points(1, 0) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(1, 1) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(1, 2) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(1, 0) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(1, 1) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(1, 2) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
 
-    REQUIRE(points(2, 0) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(2, 1) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(2, 2) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(2, 0) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(2, 1) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(2, 2) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
 
-    REQUIRE(points(3, 0) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(3, 1) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(3, 2) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(3, 0) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(3, 1) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(3, 2) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
 
-    REQUIRE(points(4, 0) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(4, 1) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(4, 2) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(4, 0) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(4, 1) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(4, 2) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
 
-    REQUIRE(points(5, 0) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(5, 1) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(5, 2) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(5, 0) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(5, 1) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(5, 2) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
 
-    REQUIRE(points(6, 0) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(6, 1) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(6, 2) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(6, 0) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(6, 1) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(6, 2) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
 
-    REQUIRE(points(7, 0) == Approx(-std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(7, 1) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
-    REQUIRE(points(7, 2) == Approx(+std::sqrt(3.) / 3.).epsilon(Tolerance));
+    REQUIRE(points(7, 0) == Approx(-1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(7, 1) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
+    REQUIRE(points(7, 2) == Approx(+1. / std::sqrt(3.)).epsilon(Tolerance));
 
     // Check weights
     auto weights = quad->weights();
@@ -126,107 +126,107 @@ TEST_CASE("Hexahedron quadratures are checked",
 
     REQUIRE(points(1, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(1, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(1, 1) == Approx(+0.).epsilon(Tolerance));
+    REQUIRE(points(1, 2) == Approx(+0.).epsilon(Tolerance));
 
     REQUIRE(points(2, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(2, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(2, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(2, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(3, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(3, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(3, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(3, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(4, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(4, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(4, 1) == Approx(+0.).epsilon(Tolerance));
+    REQUIRE(points(4, 2) == Approx(+0.).epsilon(Tolerance));
 
     REQUIRE(points(5, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(5, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(5, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(5, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(6, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(6, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(6, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(6, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(6, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(7, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(7, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(7, 1) == Approx(0.).epsilon(Tolerance));
+    REQUIRE(points(7, 2) == Approx(0.).epsilon(Tolerance));
 
     REQUIRE(points(8, 0) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(8, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(8, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-
+    REQUIRE(points(8, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    
     REQUIRE(points(9, 0) == Approx(0.).epsilon(Tolerance));
     REQUIRE(points(9, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(9, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(9, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(10, 0) == Approx(+0.).epsilon(Tolerance));
     REQUIRE(points(10, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(10, 1) == Approx(+0.).epsilon(Tolerance));
+    REQUIRE(points(10, 2) == Approx(+0.).epsilon(Tolerance));
 
     REQUIRE(points(11, 0) == Approx(+0.).epsilon(Tolerance));
     REQUIRE(points(11, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(11, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(11, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(12, 0) == Approx(+0.).epsilon(Tolerance));
     REQUIRE(points(12, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(12, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(12, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(13, 0) == Approx(+0.).epsilon(Tolerance));
     REQUIRE(points(13, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(13, 1) == Approx(+0.).epsilon(Tolerance));
+    REQUIRE(points(13, 2) == Approx(+0.).epsilon(Tolerance));
 
     REQUIRE(points(14, 0) == Approx(+0.).epsilon(Tolerance));
     REQUIRE(points(14, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(14, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(14, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(15, 0) == Approx(+0.).epsilon(Tolerance));
     REQUIRE(points(15, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(15, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(15, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(16, 0) == Approx(0.).epsilon(Tolerance));
     REQUIRE(points(16, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(16, 1) == Approx(+0.).epsilon(Tolerance));
+    REQUIRE(points(16, 2) == Approx(+0.).epsilon(Tolerance));
 
     REQUIRE(points(17, 0) == Approx(+0.).epsilon(Tolerance));
     REQUIRE(points(17, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(17, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(17, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(18, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(18, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(18, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(18, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(19, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(19, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(19, 1) == Approx(0.).epsilon(Tolerance));
+    REQUIRE(points(19, 2) == Approx(0.).epsilon(Tolerance));
 
     REQUIRE(points(20, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(20, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(20, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(20, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(21, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(21, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(21, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(21, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(22, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(22, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(22, 1) == Approx(+0.).epsilon(Tolerance));
+    REQUIRE(points(22, 2) == Approx(+0.).epsilon(Tolerance));
 
     REQUIRE(points(23, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(23, 1) == Approx(+0.).epsilon(Tolerance));
-    REQUIRE(points(23, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(23, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(24, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(24, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(24, 1) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(24, 2) == Approx(-std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     REQUIRE(points(25, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(25, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(25, 1) == Approx(+0.).epsilon(Tolerance));
+    REQUIRE(points(25, 2) == Approx(+0.).epsilon(Tolerance));
 
     REQUIRE(points(26, 0) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
     REQUIRE(points(26, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
-    REQUIRE(points(26, 1) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
+    REQUIRE(points(26, 2) == Approx(+std::sqrt(3. / 5.)).epsilon(Tolerance));
 
     // Check weights
     auto weights = quad->weights();
